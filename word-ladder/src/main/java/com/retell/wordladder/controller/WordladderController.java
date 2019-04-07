5c94347bacb96ed4c363f48bddc65d89fc30938a
Dianaaaa <1035254583@qq.com>
2019-04-07 23:29:39 +0800

    change to rest

--- word-ladder/src/main/java/com/retell/wordladder/controller/WordladderController.java
+++ word-ladder/src/main/java/com/retell/wordladder/controller/WordladderController.java
@@ -8,12 +8,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ui.Model;
 
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import java.io.*;
 
 import com.retell.wordladder.Wordladder;
 
 
-@Controller
+@RestController
 public class WordladderController {
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String index(Model model){
@@ -22,7 +29,7 @@ public class WordladderController {
     }
 
     @RequestMapping(value="/word-ladder",method= RequestMethod.GET)
-    public String outputWordLadder(String word1, String word2, Model model){
+    public String outputWordLadder(String word1, String word2){
         String worldLadder =" ";
         ClassPathResource dictPath = new ClassPathResource("static/dictionary.txt");
         try {
@@ -33,7 +40,31 @@ public class WordladderController {
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        model.addAttribute("ladder", worldLadder.toString());
-        return "index";
+
+        JSONObject result = new JSONObject();
+        result.put("wordladder", worldLadder.toString());
+        return result.toJSONString();
     }
+
+//    @RequestMapping(value="/", method=RequestMethod.GET)
+//    public String index(Model model){
+//        model.addAttribute("ladder", "Xingyue Qian");
+//        return "index";
+//    }
+//
+//    @RequestMapping(value="/word-ladder",method= RequestMethod.GET)
+//    public String outputWordLadder(String word1, String word2, Model model){
+//        String worldLadder =" ";
+//        ClassPathResource dictPath = new ClassPathResource("static/dictionary.txt");
+//        try {
+//            String dictFilePath = dictPath.getFile().getAbsolutePath();
+//            Wordladder ladder = new Wordladder(dictFilePath);
+//            worldLadder = ladder.genLadder(word1, word2);
+//        }
+//        catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        model.addAttribute("ladder", worldLadder.toString());
+//        return "index";
+//    }
 }
